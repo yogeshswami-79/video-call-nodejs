7f9da246c5869c843dcc452bea8283fe4169af7d
icmi <aluparsad79@gmail.com>
2022-06-17 17:50:02 +0530

    added animations

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,18 +1,25 @@
 const PEERS = {}
 const VIDADDED = {}
-const MAIN = "main" 
-
+const MAIN = "main"
 const socket = io("/")
 const peer = new Peer()
+let hidden = false
 
 const requestStream = async () => navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-
 
 let videoGrid = document.getElementById("video-grid")
 let myVideo = document.getElementById("myVideo")
 
+const updateVisibility = () => {
+    let anim = 'show'
+    if(hidden) anim = "fade"
+    myVideo.style.animation = `${anim} 1.2s forwards running`
+
+    hidden = !hidden
+}
+
 const addVideo = (id, stream) => {
-    if(!VIDADDED[id]){
+    if (!VIDADDED[id]) {
         console.log
         const video = document.createElement("video")
         video.addEventListener('loadedmetadata', () => video.play())
@@ -23,16 +30,17 @@ const addVideo = (id, stream) => {
 }
 
 const removeVideo = (id) => {
-    if(VIDADDED[id]) {
+    if (VIDADDED[id]) {
         VIDADDED[id].remove()
     }
 }
 
 (async () => {
-    const myVideo = document.getElementById("myVideo")
     myVideo.muted = true
     myVideo.addEventListener('loadedmetadata', () => myVideo.play())
+    myVideo.addEventListener('click', updateVisibility)
     myVideo.srcObject = await requestStream()
+
 })()
 
 
