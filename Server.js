024087df8bd508c9c6083b1929a1d8c43b609be6
icmi <aluparsad79@gmail.com>
2022-06-17 10:33:24 +0530

    added Routes, socket message event handle, code refactor

--- Server.js
+++ Server.js
@@ -2,37 +2,46 @@ const express = require("express");
 const app = express()
 const server = require("http").Server(app)
 const io = require("socket.io")(server)
-const {v4: uuidV4} = require('uuid')
+const homeRoute = require("./Routes/mainRoute")
 const PORT = process.env.PORT || 3000
-const {PeerServer} = require("peer")
+const { PeerServer } = require("peer")
+
+// Init PeerServer Instance
 PeerServer(server)
 
+// set app ViewEngine to ejs
 app.set('view engine', 'ejs')
 
+// app Static Route to public folder
 app.use(express.static('public'))
 
-app.get('/', (req, res)=>{
-    res.redirect(`/${uuidV4()}`)
-})
+// Handle main Route
+app.use("/", homeRoute)
 
-app.get('/:room',(req,res)=>{
-    res.render('room', {roomid:req.params.room})
-})
-
-io.on("connection",socket=>{
-    socket.on('join-room', (roomId,uid)=>{
-        socket.join(roomId)
-
+// Handle Socket events
+const handleSocket = socket => {
+    socket.on('join-room', (roomId, uid) => {
         console.log(`connected uid: ${uid}`)
 
+        // Join Room
+        socket.join(roomId)
+
+        // BroadCast userConnected to room
         socket.to(roomId).emit('user-connected', uid);
 
-        socket.on('disconnect', ()=>{
+        // BroadCast message event to room
+        socket.on('message', (id, data) => { socket.to(roomId).emit("msg", { id, data }) })
+
+        // ON user disconnected
+        socket.on('disconnect', () => {
             console.log(`disConnected : ${uid}`)
-            io.to(roomId).emit('user-disconnected', uid);
+            io.to(roomId).emit('user-disconnected', uid);               // Broadcast user-disconnected to room
         })
-    }) 
-})
+    })
+}
+
+// On new Socket Connection
+io.on("connection", socket => handleSocket(socket))
 
 
-server.listen(PORT, ()=>console.log(`listening on port:${PORT}`))
+server.listen(PORT, () => console.log(`listening on port:${PORT}`))
