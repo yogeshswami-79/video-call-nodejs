809f9d05ca14633b8702f3a22bbffd7defee60c4
icmi <aluparsad79@gmail.com>
2022-08-05 16:11:31 +0530

    Removed static pages and view routes

--- Server.js
+++ Server.js
@@ -2,26 +2,11 @@ const express = require("express");
 const app = express()
 const server = require("http").Server(app)
 const io = require("socket.io")(server,{cors: {origin: "*"}})
-const homeRoute = require("./Routes/mainRoute")
 const PORT = process.env.PORT || 3001
-const { PeerServer, ExpressPeerServer } = require("peer")
+const { ExpressPeerServer } = require("peer")
 
 // Init PeerServer Instance
-PeerServer({
-    hostname:"/",
-    path:'/peer',
-    port:'3002'
-})
-
-
-// set app ViewEngine to ejs
-app.set('view engine', 'ejs')
-
-// app Static Route to public folder
-app.use(express.static('public'))
-
-// Handle main Route
-// app.use("/", homeRoute)
+app.use('/peer', ExpressPeerServer(server,{debug:true}))
 
 
 // Handle Socket events
@@ -31,9 +16,6 @@ const handleSocket = socket => {
 
         // Join Room
         socket.join(roomId)
-        
-        // BroadCast message event to room
-        // socket.on('message', (id, data) => { socket.to(roomId).emit("msg", { id, data }) })
         
         // BroadCast userConnected to room
         socket.to(roomId).emit('newUser', uid);
